61695039b2a71c2e12095849782f6e089cb8e0ae
Stas <tismenetski@gmail.com>
2020-02-26 18:38:51 +0200

    * Added Form Submission

--- src/main/java/com/tismenetski/recipe/controllers/RecipeController.java
+++ src/main/java/com/tismenetski/recipe/controllers/RecipeController.java
@@ -1,12 +1,12 @@
 package com.tismenetski.recipe.controllers;
 
 
+import com.tismenetski.recipe.commands.RecipeCommand;
 import com.tismenetski.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -25,5 +25,19 @@ public class RecipeController {
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model)
+    {
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command)
+    {
+        RecipeCommand savedCommand=recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/"+ savedCommand.getId();
     }
 }
