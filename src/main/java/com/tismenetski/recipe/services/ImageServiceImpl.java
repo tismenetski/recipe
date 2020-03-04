e1f3fc5b148d721414b168b5ce4f7215253dd905
Stas <tismenetski@gmail.com>
2020-03-04 21:26:29 +0200

    * Added imageUploadForm & ImageService & ImageController

---
+++ src/main/java/com/tismenetski/recipe/services/ImageServiceImpl.java
@@ -0,0 +1,38 @@
+package com.tismenetski.recipe.services;
+
+
+import com.tismenetski.recipe.domain.Recipe;
+import com.tismenetski.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file)
+    {
+        try
+        {
+            Recipe recipe=recipeRepository.findById(recipeId).get();
+            Byte[] byteObjects=new Byte[file.getBytes().length];
+            int i=0;
+            for (byte b:file.getBytes()) byteObjects[i++]=b;
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error accured",e);
+            e.printStackTrace();
+        }
+    }
+}
